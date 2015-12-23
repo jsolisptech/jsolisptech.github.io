cb785a0850436e724acc09ce282879c47b6e17c2
jsolisptech <jsolis.ptech@gmail.com>
2015-12-23 09:36:14 -0500

    wserfe

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,12 @@
 var main = function(){
-$('.dropdown-menu').hide();
-$('.dropdown-toggle').click(function(){
+  $('.dropdown-toggle').hover(function(){
+    $('.dropdown-menu').toggle();
+  });
 
-$('.dropdown-menu').toggle();
+  $('.cell').hover(function(){
+    $('.dropdown-menu').toggle();
+  });
 
-});
+  $('.dropdown-menu').hide();
 }
 $(document).ready(main);
