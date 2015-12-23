f9f9fb00251b0a357a2b0b78994e81a1e659c2ef
jsolisptech <jsolis.ptech@gmail.com>
2015-12-23 09:32:30 -0500

    wertyuklk.,mnxdtfyguhjgbv

--- JavaScript.js
+++ JavaScript.js
@@ -4,16 +4,6 @@ $('.dropdown-toggle').click(function(){
 
 $('.dropdown-menu').toggle();
 
-
-
-}
-  $('.cell').hover(function(){
-    $('.dropdown-menu').toggle();
-
-
-    $('.dropdown-menu').hide();
-
-
 });
 }
 $(document).ready(main);
