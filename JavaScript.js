cac76ad2f81745283a5787b90da8504240175e66
jsolisptech <jsolis.ptech@gmail.com>
2015-12-23 08:54:27 -0500

    df

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,12 @@
-var main = function() {
-  $('.dropdown-menu').hide();
-  $('.dropdown-toggle').click(function(){
-    $'.dropdown-menu').toggle();
+var main = function(){
+  $('.dropdown-toggle').hover(function(){
+    $('.dropdown-menu').toggle();
   });
+
+  $('.cell').hover(function(){
+    $('.dropdown-menu').toggle();
+  });
+
+  $('.dropdown-menu').hide();
 }
-$(document).ready(main);
+(document).ready(main);
